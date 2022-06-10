6796ceb205030cb723df736fabf08043f8cd64b8
Karol Kęciński <kecinski.karol@gmail.com>
2022-06-10 08:38:36 +0200

    Updated load(), save()

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,26 @@ import fs from 'fs';
 import xml2json from 'xml2json';
 import xml2js from 'xml2js';
 
-function mapper(data : Buffer, filename : string) {
-    const json : {} = xml2json.toJson(data, { object: true });
-    console.dir(json, {depth : null, colors: true});
+function mapper(json : {}, filename : string) {
+    
 
-    let builder     = new xml2js.Builder();
-    const xml       = builder.buildObject(json)
-    console.dir(xml, {depth : null, colors: true});
-
-    save(xml, filename);
+    save(json, filename);
 }
 
 function load(file : string) {
     console.log(__dirname);
     fs.readFile(file, (err, data) => {
-        mapper(data, file);
+        const json : {} = xml2json.toJson(data, { object: true });
+        console.dir(json, {depth : null, colors: true});
+        mapper(json, file);
     })
 }
 
-function save(xml : string, filename : string) {
+function save(json : {}, filename : string) {
+    let builder     = new xml2js.Builder();
+    const xml       = builder.buildObject(json)
+    console.dir(xml, {depth : null, colors: true});
+
     fs.writeFile('PARSED_' + filename, xml, {} , (err) => {
         if (err) {
           console.log("err");
